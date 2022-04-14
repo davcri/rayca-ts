ad6626a778c8e55ec38b76d6186bc762362b59c9
Davide Cristini <davcri91@gmail.com>
2022-04-14 22:42:43 +0200

    triangle: use Vector3::copy()

--- src/rayjs/triangle.ts
+++ src/rayjs/triangle.ts
@@ -3,9 +3,9 @@ import { Object3D } from "./object3d";
 import { Vector3 } from "./vector3";
 
 export class Triangle extends Object3D {
-  v0: Vector3;
-  v1: Vector3;
-  v2: Vector3;
+  v0: Vector3 = new Vector3();
+  v1: Vector3 = new Vector3();
+  v2: Vector3 = new Vector3();
   material: Material;
 
   constructor({ position = new Vector3(), vertices = [] } = {}) {
@@ -20,9 +20,9 @@ export class Triangle extends Object3D {
       this.v1 = new Vector3(1, 0, 0);
       this.v2 = new Vector3(0, 1, 0);
     } else {
-      this.v0 = vertices[0];
-      this.v1 = vertices[1];
-      this.v2 = vertices[2];
+      this.v0.copy(vertices[0]);
+      this.v1.copy(vertices[1]);
+      this.v2.copy(vertices[2]);
     }
   }
 
