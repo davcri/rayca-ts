f7947291a735d6b45d83776c65c02ca9a74eceb6
Davide Cristini <davcri91@gmail.com>
2022-05-28 11:45:38 +0200

    vertex: rename "point" to "pos"

--- src/rayjs/triangle.ts
+++ src/rayjs/triangle.ts
@@ -35,9 +35,9 @@ export class Triangle extends Object3D {
 
   getVertices() {
     const v = [new Vector3(), new Vector3(), new Vector3()];
-    Vector3.add(this.v0.point, this.position, v[0]);
-    Vector3.add(this.v1.point, this.position, v[1]);
-    Vector3.add(this.v2.point, this.position, v[2]);
+    Vector3.add(this.v0.pos, this.position, v[0]);
+    Vector3.add(this.v1.pos, this.position, v[1]);
+    Vector3.add(this.v2.pos, this.position, v[2]);
     return v;
   }
 }
--- src/rayjs/vertex.ts
+++ src/rayjs/vertex.ts
@@ -2,11 +2,11 @@ import { Color } from "./color";
 import { Vector3 } from "./vector3";
 
 export class Vertex {
-  point: Vector3;
+  pos: Vector3;
   color: Color;
 
   constructor(x, y, z, color = new Color(1, 0, 0)) {
-    this.point = new Vector3(x, y, z);
+    this.pos = new Vector3(x, y, z);
     this.color = color;
   }
 }
